258f9753352e3b67015f7bd668bc75dcb024efd7
Miguel Raposo <migpovrap@hotmail.com>
2024-10-02 09:55:23 +0100

    add construc namedEntity

--- src/hva/core/NamedEntity.java
+++ src/hva/core/NamedEntity.java
@@ -4,6 +4,11 @@ public class NamedEntity {
     private final String _id;
     private final String _name;
 
+    public NamedEntity(String name, String id) {
+        _name = name;
+        _id = id;
+    }
+
     public String name(){
         return _name;
     }
