8bb764d950ed1c37b7a7997a690a75cb1dce4c1a
Miguel Raposo <migpovrap@hotmail.com>
2024-10-02 09:53:17 +0100

    Add namedEntity class

---
+++ src/hva/core/NamedEntity.java
@@ -0,0 +1,14 @@
+package hva.core;
+
+public class NamedEntity {
+    private final String _id;
+    private final String _name;
+
+    public String name(){
+        return _name;
+    }
+
+    public String id() {
+        return _id;
+    }
+}
