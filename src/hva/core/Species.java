ac2c5af03b488e55d12cb38be26fc5d530e53329
Miguel Raposo <migpovrap@hotmail.com>
2024-10-02 15:47:52 +0100

    class species
    has animal TreeSet

--- src/hva/core/Species.java
+++ src/hva/core/Species.java
@@ -1,12 +1,15 @@
 package hva.core;
 
 public class Species extends NamedEntity{
-
+      
+    private Collection<Animal> _animals;
+    
     public Species(String idSpecies, String name) {
         super(idSpecies, name);
+        _animals = new TreeSet<Animal>();
     }
 
     public boolean equals(Species otherSpecies) {
-        return _idSpecies.equals(otherSpecies._idSpecies);
+        return this.id.equals(otherSpecies.id);
     }
 }
