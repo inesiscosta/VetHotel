f257bf544dc1b4a75a577e9d24116f2c9bcebfa3
Miguel Raposo <migpovrap@hotmail.com>
2024-10-02 16:34:53 +0100

    animal TreeSet identifyAnimal addAnimal removeAnimal

--- src/hva/core/Habitat.java
+++ src/hva/core/Habitat.java
@@ -1,11 +1,46 @@
 package hva.core;
 
+import java.util.Collection;
+import java.util.TreeSet;;
 public class Habitat extends NamedEntity {
     private int _area;
+    private Collection<Animal> _animals;
 
     public Habitat(String idHabitat, String name, int area) {
         super(idHabitat, name);
+        _animals = new TreeSet<Animal>();
         _area = area;
+    }
+
+    protected int getNumAnimalSameSpecies(Species species) {
+
+        return null;
+    }
+
+    public int identifyInfluence(Species species) {
+        
+        return null;
+    }
+
+    protected int cleaningEffort() {
+
+        return null;
+    }
+
+    public Animal identifyAnimal(String idAnimal) {
+        for(Animal animal : _animals) {
+            if(animal.id().equals(idAnimal))
+                return a;
+        }
+        return null;
+    }
+    
+    protected void addAnimal(Animal animal) {
+        _animals.add(animal);
+    }
+
+    protected void removeAnimal(Animal animal) {
+        _animals.remove(animal);
     }
     
 }
