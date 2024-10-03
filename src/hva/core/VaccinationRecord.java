20e3955090f04dc084eedce0415cd9dba80d358e
Miguel Raposo <migpovrap@hotmail.com>
2024-10-03 17:39:59 +0100

    add connections to VaccinationRecord _administeredVaccine, _administeringVet, _vaccinatedAnimal implement toString

--- src/hva/core/VaccinationRecord.java
+++ src/hva/core/VaccinationRecord.java
@@ -2,9 +2,24 @@ package hva.core;
 
 public class VaccinationRecord {
     private HealthStatus _damage;
+    private Vaccine _administeredVaccine;
+    private Veterinary _administeringVet;
+    private Animal _vaccinatedAnimal;
 
+    @Override
     public String toString() {
-        // TODO Implement VaccinationRecord.toString
-        return "";
+        return "REGISTO-VACINA|" + _administeredVaccine.id() + "|" + _administeringVet.id() + "|" + _vaccinatedAnimal.getSpecie().id();
+    }
+
+    protected String getDamage() {
+        return _damage.toString();
+    }
+
+    protected Veterinary getVet() {
+        return _administeringVet;
+    }
+
+    protected Animal getAnimal() {
+        return _vaccinatedAnimal;
     }
 }
