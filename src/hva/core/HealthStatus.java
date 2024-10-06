21a0ef4b07d9b899c77915e11c24bae2c9cb07a4
Inês Costa <inesiscosta@tecnico.ulisboa.pt>
2024-10-07 00:46:04 +0100

    Even more docs + fixing indentation to match professor's 2 space indent

--- src/hva/core/HealthStatus.java
+++ src/hva/core/HealthStatus.java
@@ -1,21 +1,32 @@
 package hva.core;
 import hva.core.exception.InvalidDamageValueException;
 
+/**
+ * Enumerates the possible health status of an animal / effects of a vaccine on an animal.
+ */
 public enum HealthStatus {
-    NORMAL,
-    CONFUSION,
-    ACCIDENT,
-    ERROR;
-
-    protected static HealthStatus determineHealthStatus(int damage, boolean correctSpecies) throws InvalidDamageValueException{
-        if (correctSpecies)
-            return HealthStatus.NORMAL;
-        if (damage == 0)
-            return HealthStatus.CONFUSION;
-        else if (damage >= 1 && damage <= 4)
-            return HealthStatus.ACCIDENT;
-        else if (damage >= 5)
-            return HealthStatus.ERROR;
-        throw new InvalidDamageValueException(damage);
+  NORMAL,
+  CONFUSION,
+  ACCIDENT,
+  ERROR;
+  
+  /**
+   * Determines the health status of an animal based on the damage and if the species is correct.
+   * 
+   * @param damage the damage suffered by the animal as the result of a vaccine
+   * @param correctSpecies a boolean indicating whether the species is correct
+   * @return the animal's health status (effect on animal's health) as a result of the vaccine
+   * @throws InvalidDamageValueException if the damage is invalid (less than 0)
+   */
+  protected static HealthStatus determineHealthStatus(int damage, boolean correctSpecies) throws InvalidDamageValueException{
+    if (correctSpecies)
+      return HealthStatus.NORMAL;
+    else if (damage == 0)
+      return HealthStatus.CONFUSION;
+    else if (damage >= 1 && damage <= 4)
+      return HealthStatus.ACCIDENT;
+    else if (damage >= 5)
+      return HealthStatus.ERROR;
+    throw new InvalidDamageValueException(damage);
     }
 }
--- src/hva/core/Leaf.java
+++ src/hva/core/Leaf.java
@@ -1,8 +1,11 @@
 package hva.core;
 
+/**
+ * Enumeration of the possible states of the leaves on a tree.
+ */
 public enum Leaf {
-    WITH_LEAVES,
-    WITHOUT_LEAVES,
-    GENERATING_LEAVES,
-    SHEDDING_LEAVES;
+  WITH_LEAVES,
+  WITHOUT_LEAVES,
+  GENERATING_LEAVES,
+  SHEDDING_LEAVES;
 }
