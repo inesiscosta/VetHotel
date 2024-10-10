c529593484b1e08896e1c04542e49e14f4ffc97d
Miguel Raposo <migpovrap@hotmail.com>
2024-10-10 14:10:11 +0100

    employee changed employeetype enum

--- src/hva/core/Employee.java
+++ src/hva/core/Employee.java
@@ -48,7 +48,7 @@ public abstract class Employee extends NamedEntity {
   @Override
   public String toString() {
     StringBuilder result = new StringBuilder();
-    result.append(EmployeeType.enumToString(this.type()))
+    result.append(this.type().pt())
       .append("|")
       .append(this.id())
       .append("|")
