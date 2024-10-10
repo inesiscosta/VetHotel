0aae2f786a555c939c937b02bb15eb2e813de3fc
Inês Costa <inesiscosta@tecnico.ulisboa.pt>
2024-10-10 14:10:11 +0100

    Employee Type Fix

--- src/hva/core/EmployeeType.java
+++ src/hva/core/EmployeeType.java
@@ -4,29 +4,16 @@ package hva.core;
  * Enumerates the types of employees that can work for a Vet Hotel.
  */
 public enum EmployeeType {
-  VETERINARIAN,
-  ZOOKEEPER;
+  VETERINARIAN ("VET"),
+  ZOOKEEPER ("TRT");
 
-  /**
-     * A method for returning the employee type representation used by the app layer
-     *
-     * @param type The enum of EmployeeType
-     * @return String that correspond to the internal employee type representation
-     */
-    public static String enumToString(EmployeeType type) {  //FIXME Ines temos de ver ser fazemos isto ou other way maybe override valueOf? 
-      String employeeType = null;
-      switch (type) {
-          case VETERINARIAN:
-            employeeType = "VET";
-            return employeeType;
+  private final String _pt;
 
-          case ZOOKEEPER:
-              employeeType = "TRT";
-              return employeeType;
-
-          default:
-              return employeeType;
-      }
+  EmployeeType(String pt) {
+    this._pt = pt;
   }
 
+  public String pt() {
+    return _pt;
+  }
 }
