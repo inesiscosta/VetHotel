4ac33863851461403b02388eb55743478138283e
Miguel Raposo <migpovrap@hotmail.com>
2024-10-20 18:47:23 +0100

    satisfaction interface implements Serlializable (Duh major oversight)

--- src/hva/core/satisfactionStrategy/Satisfaction.java
+++ src/hva/core/satisfactionStrategy/Satisfaction.java
@@ -3,11 +3,12 @@ package hva.core.satisfactionStrategy;
 import hva.core.Veterinarian;
 import hva.core.ZooKeeper;
 
+import java.io.Serializable;
 /**
  * Interface for the Satisfaction calculation, can have multiple methods of
  * doing it.
  */
-public interface Satisfaction {
+public interface Satisfaction extends Serializable{
     double calculateSatisfaction(Veterinarian vet);
     double calculateSatisfaction(ZooKeeper keeper);
 }
