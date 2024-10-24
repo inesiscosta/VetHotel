a9582bf39a39885646f6ff94095c63ec8257338a
Miguel Raposo <migpovrap@hotmail.com>
2024-10-25 00:10:26 +0100

    Final remove TODO

--- src/hva/app/habitat/DoChangeHabitatInfluence.java
+++ src/hva/app/habitat/DoChangeHabitatInfluence.java
@@ -28,7 +28,6 @@ class DoChangeHabitatInfluence extends Command<Hotel> {
     var idSpecies = stringField("idSpecies");
     var influence = optionField("influenceString");
     try {
-      //TODO: toString not in Core create String in App, if we do this should use a HashMap for everything in the core.
       _receiver.changeHabitatInfluence(idHabitat, idSpecies, influence);
     } catch (UnknownHabitatIdException e) {
       throw new UnknownHabitatKeyException(idHabitat);
